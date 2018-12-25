95a41e39d3a1bc90bf1f6da398999d32c782fc45
guohong <guohong@zhphfinance.com>
2018-12-25 09:47:02 +0800

    添加eureka注册中心，dept服务提供者集成eureka

--- microservicecloud-provider-dept-8001/src/main/java/com/gh/springcloud/DeptProvider8001_App.java
+++ microservicecloud-provider-dept-8001/src/main/java/com/gh/springcloud/DeptProvider8001_App.java
@@ -2,8 +2,10 @@ package com.gh.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class DeptProvider8001_App
 {
 	public static void main(String[] args)
